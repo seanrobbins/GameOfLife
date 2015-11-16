3c81118fa742f077577a7b2663a40858b33812ad
Sean Robbins <serobbins@expedia.com>
2015-11-16 01:08:37 +0000

    Move tick logic into Grid and remove Game type completely. Game seemed to serve no useful function.

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,4 +1,29 @@
-class Grid(val vector: Vector[Vector[Cell]])
+import scala.annotation.tailrec
+
+class Grid(val vector: Vector[Vector[Cell]]) {
+  def tick: Grid = {
+    @tailrec
+    def nextGenerationHelper(v: Vector[Vector[Cell]], r: Vector[Vector[Cell]], x: Int): Vector[Vector[Cell]] = {
+      if(v.isEmpty) r
+      else nextGenerationHelper(v.tail, r ++ Vector(nextGenerationOfXAxis(v.head, x)), x + 1)
+    }
+
+    def nextGenerationOfXAxis(v: Vector[Cell], x: Int): Vector[Cell] = {
+      @tailrec
+      def nextGenerationOfYAxis(v: Vector[Cell], r: Vector[Cell], y: Int): Vector[Cell] = {
+        if(v.isEmpty) r
+        else nextGenerationOfYAxis(v.tail,
+          r ++ Vector(Cell.rules(v.head, Grid.getCellLivingNeighbours(this)(x, y).size)),
+          y + 1)
+      }
+
+      nextGenerationOfYAxis(v, Vector(), 0)
+    }
+
+    new Grid(nextGenerationHelper(vector, Vector(), 0))
+  }
+
+}
 
 object Grid {
   def initialize(x: Int, y: Int): Grid = new Grid(Vector.fill[Cell](x, y) { Dead })
--- src/test/scala/GridSpec.scala
+++ src/test/scala/GridSpec.scala
@@ -1,5 +1,6 @@
 import org.scalatest.{Inside, Matchers, FunSpecLike}
 
+import scala.annotation.tailrec
 import scala.util.Random
 
 class GridSpec extends FunSpecLike with Matchers with Inside {
@@ -63,5 +64,28 @@ class GridSpec extends FunSpecLike with Matchers with Inside {
     val grid = Grid.setCell(Grid.initialize(5, 5))(4, 4)(Alive)
     val result = Grid.getCell(grid)(4, 4)
     result should be(Alive)
+  }
+
+  describe("#tick") {
+    it("should return a grid with a grid one dead cell from a game with a single grid with one alive cell") {
+      val grid = Grid.setCell(Grid.initialize(1,1))(0,0)(Alive)
+      val result = grid.tick
+      Grid.getCell(result)(0,0) should be(Dead)
+    }
+
+    it("should provide the game with live and dead cells updated") {
+      val livingCells: Vector[(Int, Int)] = Vector((0,0), (0,1), (1,1))
+      @tailrec
+      def seed(g: Grid, lc: Vector[(Int, Int)]): Grid = {
+        if(lc.isEmpty) g
+        else seed(Grid.setCell(g)(lc.head._1, lc.head._2)(Alive), lc.tail)
+      }
+      val grid = seed(Grid.initialize(4, 4), livingCells)
+      val result = grid.tick
+      Grid.getCell(result)(0,0) should be(Alive)
+      Grid.getCell(result)(0,1) should be(Alive)
+      Grid.getCell(result)(1,0) should be(Alive)
+      Grid.getCell(result)(1,1) should be(Alive)
+    }
   }
 }
